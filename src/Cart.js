a3c04ea1d1397b814b028bfd10198a134301383e
usama khan <usamakhan21gb@gmail.com>
2022-12-02 11:22:34 +0500

    complete

--- src/Cart.js
+++ src/Cart.js
@@ -1,9 +1,13 @@
-import React from "react";
-import { Box, Typography } from "@mui/material";
+import React, { useEffect } from "react";
+import { Box, Button, Typography } from "@mui/material";
 import CartItem from "./Components/CartItem";
 import { connect } from "react-redux";
 
-const CartContainer = ({ cart = [] }) => {
+const CartContainer = ({ cart = [], total, dispatch }) => {
+  useEffect(() => {
+    dispatch({ type: "GET_TOTALS" });
+  }, [dispatch, cart]);
+
   if (cart.length === 0) {
     return (
       <Box component="div" sx={{ width: "90vw", margin: "40px auto" }}>
@@ -38,12 +42,32 @@ const CartContainer = ({ cart = [] }) => {
       {cart.map((item, i) => {
         return <CartItem key={i} {...item} />;
       })}
+      <hr />
+      <Box
+        component="div"
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-around",
+          padding: "20px",
+        }}
+      >
+        <Button
+          variant="outlined"
+          color="error"
+          onClick={() => dispatch({ type: "CLEAR_CART" })}
+        >
+          Clear Cart
+        </Button>
+        <Typography>${total}</Typography>
+      </Box>
     </Box>
   );
 };
 
 function mapStateToProps(state) {
-  return { cart: state.cart };
+  let { cart, total } = state;
+  return { total, cart };
 }
 
 export default connect(mapStateToProps)(CartContainer);
--- src/reducer/CartReducer.js
+++ src/reducer/CartReducer.js
@@ -1,4 +1,7 @@
 export const reducer = (state, { type, payload }) => {
+  if (type === "CLEAR_CART") {
+    return { ...state, cart: [] };
+  }
   if (type === "REMOVE") {
     return {
       ...state,
@@ -19,14 +22,31 @@ export const reducer = (state, { type, payload }) => {
     if (payload.amount === 1) {
       tempCart = state.cart.filter((cartItem) => cartItem.id !== payload.id);
     } else {
-       tempCart = state.cart.map((cartItem) => {
+      tempCart = state.cart.map((cartItem) => {
         if (cartItem.id === payload.id) {
           return { ...cartItem, amount: cartItem.amount - 1 };
         }
         return cartItem;
-      }); 
+      });
     }
     return { ...state, cart: tempCart };
+  }
+  if (type === "GET_TOTALS") {
+    let { amount, total } = state.cart.reduce(
+      (cartTotal, cartItem) => {
+        let { price, amount } = cartItem;
+        let itemTotal = price * amount;
+        cartTotal.total += itemTotal;
+        cartTotal.amount += amount;
+        return cartTotal;
+      },
+      {
+        amount: 0,
+        total: 0,
+      }
+    );
+    total = total.toFixed(2);
+    return { ...state, amount, total };
   }
   return state;
 };
